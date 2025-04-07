d415a889606729dc8d09e94f51d85afdfa728222
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-08 01:19:52 +0300

    Add models for images, place

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Place, Image
 
-# Register your models here.
+@admin.register(Place)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['title', 'id', ]
+    search_fields = ['title', ]
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = [ 'place', 'ordinal_number']
+    list_editable = ['ordinal_number', ]
+
--- places/models.py
+++ places/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+    place = models.ForeignKey('Place', on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='images/')
+    ordinal_number = models.IntegerField(unique=True)
+
+    def __str__(self):
+        return f'{self.place} - {self.ordinal_number}'
+
+
+class Place(models.Model):
+    title = models.CharField(max_length=50)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    lng = models.FloatField()
+    lat = models.FloatField()
+
+    def __str__(self):
+        return self.title
