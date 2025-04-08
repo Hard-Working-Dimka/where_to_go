6acb7aa3b9aec447141600055a5c3bb805e9c4be
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-09 00:03:10 +0300

    add inline for images in places

--- places/admin.py
+++ places/admin.py
@@ -1,14 +1,23 @@
 from django.contrib import admin
 from .models import Place, Image
 
+class ImageInline(admin.TabularInline):
+    model = Image
+
+
+
 @admin.register(Place)
 class EventAdmin(admin.ModelAdmin):
     list_display = ['title', 'id', ]
     search_fields = ['title', ]
+    inlines = [
+        ImageInline,
+    ]
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
     list_display = [ 'place', 'ordinal_number']
     list_editable = ['ordinal_number', ]
     search_fields = ['place', ]
+
 
