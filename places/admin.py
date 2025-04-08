06c2aaf923b3144a38078d3bb53a1090286f9a94
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-09 00:43:12 +0300

    add preview for images

--- places/admin.py
+++ places/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
+
 
 class ImageInline(admin.TabularInline):
     model = Image
+    readonly_fields = ['place_image', ]
 
+    def place_image(self, obj):
+        height = obj.image.height
+        width = obj.image.width
+        if height > 200:
+            height = 200
+        if width > 200:
+            width = 200 #TODO: посмотреть с лямбдой
+
+        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
+            url=obj.image.url,
+            width=width,
+
+            height=height
+        )
+        )
 
 
 @admin.register(Place)
@@ -14,10 +33,9 @@ class EventAdmin(admin.ModelAdmin):
         ImageInline,
     ]
 
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = [ 'place', 'ordinal_number']
+    list_display = ['place', 'ordinal_number',]
     list_editable = ['ordinal_number', ]
     search_fields = ['place', ]
-
-
