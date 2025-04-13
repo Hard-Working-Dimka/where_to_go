0562e2bd3c20baa6e97c6780fe79c1aa5adb2973
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2025-04-14 01:43:28 +0300

    reduce the code for images

--- places/admin.py
+++ places/admin.py
@@ -4,6 +4,9 @@ from django.utils.html import format_html
 from .models import Place, Image
 from adminsortable2.admin import SortableStackedInline, SortableAdminBase
 
+IMAGE_MAX_WIDTH = 200
+IMAGE_MAX_HEIGHT = 200
+
 
 class ImageInline(SortableStackedInline):
     model = Image
@@ -11,11 +14,14 @@ class ImageInline(SortableStackedInline):
     extra = 10
 
     def place_image(self, obj):
-        return format_html('<img src="{url}" width="{width}" height={height} />'.format(
-            url=obj.image.url,
-            width=(lambda x: 200 if x > 200 else x)(obj.image.width),
-            height=(lambda x: 200 if x > 200 else x)(obj.image.height)
-        )
+        return format_html(
+            '<img src="{url}" width="{width}" height={height} style="max-width:{max_width}px; max-height:{max_height}px" />'.format(
+                url=obj.image.url,
+                width=obj.image.width,
+                height=obj.image.height,
+                max_width=IMAGE_MAX_WIDTH,
+                max_height=IMAGE_MAX_HEIGHT
+            )
         )
 
 
@@ -27,9 +33,10 @@ class EventAdmin(SortableAdminBase, admin.ModelAdmin):
         ImageInline,
     ]
 
+
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['place', 'ordinal_number',]
+    list_display = ['place', 'ordinal_number', ]
     list_editable = ['ordinal_number', ]
     search_fields = ['place', ]
     autocomplete_fields = ['place', ]
